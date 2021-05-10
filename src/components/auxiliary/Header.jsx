c2d4fc6988769fb944ad5c0456774d5e311a9e56
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-05-10 18:49:44 -0300

    Fetch of the movie in MovieDetails

--- src/components/auxiliary/Header.jsx
+++ src/components/auxiliary/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import logo from './jonnesflix-logo.png';
 
 class Header extends React.Component {
   render() {
     return (
       <header className="movie-card-header">
-        <img className="page-title" src="images/jonnesflix-logo.png" alt="logo" />
+        <img className="page-title" src={ logo } alt="logo" />
       </header>
     );
   }
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-// import * as movieAPI from '../services/movieAPI';
-// import { Loading } from '../components';
+import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movie: [],
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  fetchMovie = async () => {
+    const { match: { params: { id } } } = this.props;
+    console.log(id);
+    const movieRequired = await movieAPI.getMovie(id);
+    console.log(movieRequired);
+    this.setState({
+      movie: movieRequired,
+      loading: false,
+    });
+  }
+
   render() {
     // Change the condition to check the state
-    // if (true) return <Loading />;
+    const { loading } = this.state;
+    if (loading) return <Loading />;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    const { movie } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
 
     return (
       <div data-testid="movie-details">
@@ -22,5 +49,13 @@ class MovieDetails extends Component {
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default MovieDetails;
