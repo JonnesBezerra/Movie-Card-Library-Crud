7d9b3c261a02f0f06a374c7d212d57b5e2e2865e
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-05-13 14:16:48 -0300

    FixoStyle: Loading...

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class Loading extends Component {
   render() {
     return (
-      <section className="loading">
-        <p>Carregando...</p>
+      <section className="loading-section">
+        <p className="loading">Carregando...</p>
       </section>
     );
   }
